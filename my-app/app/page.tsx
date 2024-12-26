3082d9078c58bfb12f1ba685ede29472309e64b8
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2024-12-27 00:36:21 +0500

    code correction

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,6 @@
 // import Image from 'next/image';
 import Head from 'next/head';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import NewArrivals from './components/NewArrivals';
 import TopSelling from './components/TopSelling';
